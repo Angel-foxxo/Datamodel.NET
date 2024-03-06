ab2e6a1cb8d0c44eb7b847c5fe401c652f9ba583
Kristi K <kristik06@outlook.com>
2024-03-06 15:59:43 +0100

    Change Color, QAngle to record struct

--- Types/Color.cs
+++ Types/Color.cs
@@ -2,43 +2,16 @@ using System;
 
 namespace Datamodel;
 
-public struct Color : IEquatable<Color>
+public record struct Color(byte R, byte G, byte B, byte A)
 {
-    public byte R;
-    public byte G;
-    public byte B;
-    public byte A;
-
-    public Color(byte r, byte g, byte b, byte a)
-    {
-        R = r;
-        G = g;
-        B = b;
-        A = a;
-    }
-
-    public static bool operator ==(Color first, Color second)
-        => first.R == second.R && first.G == second.G && first.B == second.B && first.A == second.A;
-
-    public static bool operator !=(Color a, Color b)
-        => !(a == b);
-
     public static Color FromBytes(ReadOnlySpan<byte> bytes)
         => new(bytes[0], bytes[1], bytes[2], bytes[3]);
 
+    public void ToBytes(Span<byte> bytes)
+    {
+        bytes[0] = R; bytes[1] = G; bytes[2] = B; bytes[3] = A;
+    }
+
     public byte[] ToBytes()
         => new byte[] { R, G, B, A };
-
-    public override int GetHashCode()
-        => HashCode.Combine(R, G, B, A);
-
-    public bool Equals(Color other)
-        => this == other;
-
-    public override bool Equals(object obj)
-    {
-        if (obj is Color color)
-            return this == color;
-        return false;
-    }
 }
--- Types/QAngle.cs
+++ Types/QAngle.cs
@@ -3,38 +3,8 @@ using System.Numerics;
 
 namespace Datamodel;
 
-public struct QAngle : IEquatable<QAngle>
+public record struct QAngle(float Pitch, float Yaw, float Roll)
 {
-    public float Pitch;
-    public float Yaw;
-    public float Roll;
-
-    public QAngle(float pitch, float yaw, float roll)
-    {
-        Pitch = pitch;
-        Yaw = yaw;
-        Roll = roll;
-    }
-
     public static implicit operator Vector3(QAngle q) => new(q.Pitch, q.Yaw, q.Roll);
     public static implicit operator QAngle(Vector3 v) => new(v.X, v.Y, v.Z);
-
-    public static bool operator ==(QAngle first, QAngle second)
-        => first.Pitch == second.Pitch && first.Yaw == second.Yaw && first.Roll == second.Roll;
-
-    public static bool operator !=(QAngle a, QAngle b)
-        => !(a == b);
-
-    public readonly override int GetHashCode()
-        => HashCode.Combine(Pitch, Yaw, Roll);
-
-    public readonly bool Equals(QAngle other)
-        => this == other;
-
-    public readonly override bool Equals(object obj)
-    {
-        if (obj is QAngle q)
-            return this == q;
-        return false;
-    }
 }
